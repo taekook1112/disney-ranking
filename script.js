0dc1f646ad49ab84daac129a79f34792c53394f3
taekook1112 <e2127419@soka-u.jp>
2025-07-13 23:11:28 +0900

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-function sortList() {
-    const container = document.getElementById("ranking-container");
-    const cards = Array.from(container.getElementsByClassName("card"));
-    cards.sort((a, b) => {
-        return a.dataset.rank - b.dataset.rank;
-    });
-    cards.reverse().forEach(card => container.appendChild(card));
-}
